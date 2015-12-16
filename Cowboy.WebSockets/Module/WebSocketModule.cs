7cedd93fc6a706c21bcc2901e39ce1ac16005fba
gaochundong@hotmail.com <gaochundong@hotmail.com>
2015-12-16 12:19:14 +0800

    implemented the broadcast and sent to interfaces

--- Cowboy.WebSockets/Module/WebSocketModule.cs
+++ Cowboy.WebSockets/Module/WebSocketModule.cs
@@ -11,7 +11,7 @@ namespace Cowboy.WebSockets
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private static readonly Regex ModuleNameExpression = new Regex(@"(?<name>[\w]+)Module$", RegexOptions.Compiled);
 
-        private ConcurrentDictionary<IPEndPoint, WebSocketSession> _sessions = new ConcurrentDictionary<IPEndPoint, WebSocketSession>();
+        private ConcurrentDictionary<string, WebSocketSession> _sessions = new ConcurrentDictionary<string, WebSocketSession>();
 
         protected WebSocketModule()
             : this(string.Empty)
@@ -40,16 +40,60 @@ namespace Cowboy.WebSockets
 
         public async Task AcceptSession(WebSocketSession session)
         {
-            if (_sessions.TryAdd(session.RemoteEndPoint, session))
+            if (_sessions.TryAdd(session.RemoteEndPoint.ToString(), session))
             {
                 await session.Start();
 
                 WebSocketSession throwAway;
-                _sessions.TryRemove(session.RemoteEndPoint, out throwAway);
+                _sessions.TryRemove(session.RemoteEndPoint.ToString(), out throwAway);
             }
         }
 
         public abstract Task ReceiveTextMessage(WebSocketTextMessage message);
         public abstract Task ReceiveBinaryMessage(WebSocketBinaryMessage message);
+
+        public async Task Broadcast(string text)
+        {
+            foreach (var session in _sessions.Values)
+            {
+                await session.Send(text);
+            }
+        }
+
+        public async Task Broadcast(byte[] binary)
+        {
+            await Broadcast(binary, 0, binary.Length);
+        }
+
+        public async Task Broadcast(byte[] binary, int offset, int count)
+        {
+            foreach (var session in _sessions.Values)
+            {
+                await session.Send(binary, offset, count);
+            }
+        }
+
+        public async Task SendTo(string endpoint, string text)
+        {
+            WebSocketSession session;
+            if (_sessions.TryGetValue(endpoint, out session))
+            {
+                await session.Send(text);
+            }
+        }
+
+        public async Task SendTo(string endpoint, byte[] binary)
+        {
+            await SendTo(endpoint, binary, 0, binary.Length);
+        }
+
+        public async Task SendTo(string endpoint, byte[] binary, int offset, int count)
+        {
+            WebSocketSession session;
+            if (_sessions.TryGetValue(endpoint, out session))
+            {
+                await session.Send(binary, offset, count);
+            }
+        }
     }
 }
