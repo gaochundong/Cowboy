3ef5c8df82a6663c84ccb00947e0658b16fbe95a
gaochundong@hotmail.com <gaochundong@hotmail.com>
2015-12-17 19:59:42 +0800

    fixed the bug to throw invalid operation exception

--- Cowboy.WebSockets/Buffer/GrowingByteBufferManager.cs
+++ Cowboy.WebSockets/Buffer/GrowingByteBufferManager.cs
@@ -66,7 +66,7 @@ namespace Cowboy.WebSockets
         {
             lock (_sync)
             {
-                if (_bufferStack.Peek() != null)
+                if (_bufferStack.Count > 0)
                     return _bufferStack.Pop();
 
                 if (AutoExpand)
