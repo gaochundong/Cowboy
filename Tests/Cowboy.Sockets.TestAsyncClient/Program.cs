11c334093692674f5859dff8d290eac978f3e6ce
gaochundong@hotmail.com <gaochundong@hotmail.com>
2015-12-25 17:49:23 +0800

    disable ssl by default

--- Tests/Cowboy.Sockets.TestAsyncClient/Program.cs
+++ Tests/Cowboy.Sockets.TestAsyncClient/Program.cs
@@ -16,10 +16,10 @@ namespace Cowboy.Sockets.TestAsyncClient
             NLogLogger.Use();
 
             var config = new AsyncTcpSocketClientConfiguration();
-            config.UseSsl = true;
-            config.SslTargetHost = "Cowboy";
-            config.SslClientCertificates.Add(new X509Certificate2(@"D:\\Cowboy.cer"));
-            config.SslPolicyErrorsBypassed = false;
+            //config.UseSsl = true;
+            //config.SslTargetHost = "Cowboy";
+            //config.SslClientCertificates.Add(new X509Certificate2(@"D:\\Cowboy.cer"));
+            //config.SslPolicyErrorsBypassed = false;
 
             IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 22222);
             IPEndPoint localEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 22221);
--- Tests/Cowboy.Sockets.TestAsyncServer/Program.cs
+++ Tests/Cowboy.Sockets.TestAsyncServer/Program.cs
@@ -15,9 +15,9 @@ namespace Cowboy.Sockets.TestAsyncServer
             NLogLogger.Use();
 
             var config = new AsyncTcpSocketServerConfiguration();
-            config.UseSsl = true;
-            config.SslServerCertificate = new X509Certificate2(@"D:\\Cowboy.pfx", "Cowboy");
-            config.SslPolicyErrorsBypassed = false;
+            //config.UseSsl = true;
+            //config.SslServerCertificate = new X509Certificate2(@"D:\\Cowboy.pfx", "Cowboy");
+            //config.SslPolicyErrorsBypassed = false;
 
             _server = new AsyncTcpSocketServer(22222, new SimpleMessageDispatcher(), config);
             _server.Start();
