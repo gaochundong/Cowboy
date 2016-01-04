a214937ae21bfc0320a11f7dd0266b2043dbd09c
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-01-04 17:06:00 +0800

    delete the non-implemented projects

--- Cowboy.Statistics.StatsDIntegration/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Cowboy.Statistics.StatsDIntegration")]
-[assembly: Guid("44138798-e82f-4899-b20c-c8f59ce34463")]
--- Cowboy.Statistics/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Cowboy.Statistics")]
-[assembly: Guid("7440ac6d-b7b9-49e3-9e7d-1392204f314f")]
