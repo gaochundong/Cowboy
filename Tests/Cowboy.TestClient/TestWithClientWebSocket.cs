0ef1f762305901d8eddc8155b87570cf8832ae37
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-01-05 15:14:00 +0800

    modify the test module name

--- Tests/Cowboy.TestClient/TestWithClientWebSocket.cs
+++ Tests/Cowboy.TestClient/TestWithClientWebSocket.cs
@@ -12,7 +12,7 @@ namespace Cowboy.TestClient
 
         public static async Task Connect()
         {
-            string uri = "ws://localhost:3202/test";
+            string uri = "ws://localhost:3202/";
 
             ClientWebSocket webSocket = null;
 
