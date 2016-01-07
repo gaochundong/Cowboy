c1f1177bd3b3971a4d96ef018878c1de8f274f49
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-01-07 17:33:03 +0800

    catch timeout exception to test

--- Tests/Cowboy.Sockets.TestAsyncClient/Program.cs
+++ Tests/Cowboy.Sockets.TestAsyncClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Cowboy.Logging;
 using Cowboy.Logging.NLogIntegration;
 
 namespace Cowboy.Sockets.TestAsyncClient
@@ -14,39 +15,46 @@ namespace Cowboy.Sockets.TestAsyncClient
         {
             NLogLogger.Use();
 
-            var config = new AsyncTcpSocketClientConfiguration();
-            //config.UseSsl = true;
-            //config.SslTargetHost = "Cowboy";
-            //config.SslClientCertificates.Add(new System.Security.Cryptography.X509Certificates.X509Certificate2(@"D:\\Cowboy.cer"));
-            //config.SslPolicyErrorsBypassed = false;
-
-            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 22222);
-            _client = new AsyncTcpSocketClient(remoteEP, new SimpleMessageDispatcher(), config);
-            _client.Connect().Wait();
-
-            Console.WriteLine("TCP client has connected to server [{0}].", remoteEP);
-            Console.WriteLine("Type something to send to server...");
-            while (true)
+            try
             {
-                try
-                {
-                    string text = Console.ReadLine();
-                    if (text == "quit")
-                        break;
-                    Task.Run(async () =>
-                    {
-                        await _client.Send(Encoding.UTF8.GetBytes(text));
-                        Console.WriteLine("Client [{0}] send data -> [{1}].", _client.LocalEndPoint, text);
-                    });
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
+                var config = new AsyncTcpSocketClientConfiguration();
+                //config.UseSsl = true;
+                //config.SslTargetHost = "Cowboy";
+                //config.SslClientCertificates.Add(new System.Security.Cryptography.X509Certificates.X509Certificate2(@"D:\\Cowboy.cer"));
+                //config.SslPolicyErrorsBypassed = false;
 
-            _client.Close();
-            Console.WriteLine("TCP client has disconnected from server [{0}].", remoteEP);
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 22222);
+                _client = new AsyncTcpSocketClient(remoteEP, new SimpleMessageDispatcher(), config);
+                _client.Connect().Wait();
+
+                Console.WriteLine("TCP client has connected to server [{0}].", remoteEP);
+                Console.WriteLine("Type something to send to server...");
+                while (true)
+                {
+                    try
+                    {
+                        string text = Console.ReadLine();
+                        if (text == "quit")
+                            break;
+                        Task.Run(async () =>
+                        {
+                            await _client.Send(Encoding.UTF8.GetBytes(text));
+                            Console.WriteLine("Client [{0}] send data -> [{1}].", _client.LocalEndPoint, text);
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                _client.Close();
+                Console.WriteLine("TCP client has disconnected from server [{0}].", remoteEP);
+            }
+            catch (Exception ex)
+            {
+                Logger.Get<Program>().Error(ex.Message, ex);
+            }
 
             Console.ReadKey();
         }
