f0d854142c358a444a72e0960a5c50a5b835106d
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-01-08 17:42:14 +0800

    added test projects

---
+++ Tests/Cowboy.Sockets.WebSockets.TestAsyncWebSocketClient/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Cowboy.Logging;
+using Cowboy.Logging.NLogIntegration;
+
+namespace Cowboy.Sockets.WebSockets.TestAsyncWebSocketClient
+{
+    class Program
+    {
+        static AsyncWebSocketClient _client;
+
+        static void Main(string[] args)
+        {
+            NLogLogger.Use();
+
+            try
+            {
+                var config = new AsyncWebSocketClientConfiguration();
+                config.SslTargetHost = "Cowboy";
+                config.SslClientCertificates.Add(new System.Security.Cryptography.X509Certificates.X509Certificate2(@"D:\\Cowboy.cer"));
+                config.SslPolicyErrorsBypassed = false;
+
+                Uri uri = new Uri("ws://54.223.158.99:80/");
+                _client = new AsyncWebSocketClient(uri, OnServerDataReceived, OnServerConnected, OnServerDisconnected, config);
+                _client.Connect().Wait();
+
+                Console.WriteLine("WebSocket client has connected to server [{0}].", uri);
+                Console.WriteLine("Type something to send to server...");
+                while (true)
+                {
+                    try
+                    {
+                        string text = Console.ReadLine();
+                        if (text == "quit")
+                            break;
+                        Task.Run(async () =>
+                        {
+                            await _client.Send(Encoding.UTF8.GetBytes(text));
+                            Console.WriteLine("Client [{0}] send data -> [{1}].", _client.LocalEndPoint, text);
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                _client.Close();
+                Console.WriteLine("WebSocket client has disconnected from server [{0}].", uri);
+            }
+            catch (Exception ex)
+            {
+                Logger.Get<Program>().Error(ex.Message, ex);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static async Task OnServerConnected(AsyncWebSocketClient client)
+        {
+            Console.WriteLine(string.Format("WebSocket server {0} has connected.", client.RemoteEndPoint));
+            await Task.CompletedTask;
+        }
+
+        private static async Task OnServerDataReceived(AsyncWebSocketClient client, byte[] data, int offset, int count)
+        {
+            var text = Encoding.UTF8.GetString(data, offset, count);
+            Console.Write(string.Format("WebSocket server : {0} --> ", client.RemoteEndPoint));
+            Console.WriteLine(string.Format("{0}", text));
+
+            await Task.CompletedTask;
+        }
+
+        private static async Task OnServerDisconnected(AsyncWebSocketClient client)
+        {
+            Console.WriteLine(string.Format("WebSocket server {0} has disconnected.", client.RemoteEndPoint));
+            await Task.CompletedTask;
+        }
+    }
+}
