e26a60206317fcdcc48de9a34044a68be5eaa7c1
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-01-12 10:11:23 +0800

    create the new web socket test server

---
+++ Tests/Cowboy.Sockets.WebSockets.TestAsyncWebSocketServer/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Cowboy.Logging;
+using Cowboy.Logging.NLogIntegration;
+
+namespace Cowboy.Sockets.WebSockets.TestAsyncWebSocketServer
+{
+    class Program
+    {
+        static AsyncWebSocketServer _server;
+
+        static void Main(string[] args)
+        {
+            NLogLogger.Use();
+
+            try
+            {
+                var config = new AsyncWebSocketServerConfiguration();
+                //config.UseSsl = true;
+                //config.SslServerCertificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(@"D:\\Cowboy.pfx", "Cowboy");
+                //config.SslPolicyErrorsBypassed = false;
+                config.Masking = false;
+
+                _server = new AsyncWebSocketServer(22222);
+                _server.Start().Wait();
+
+                Console.WriteLine("WebSocket server has been started on [{0}].", _server.ListenedEndPoint);
+                Console.WriteLine("Type something to send to clients...");
+                while (true)
+                {
+                    try
+                    {
+                        string text = Console.ReadLine();
+                        if (text == "quit")
+                            break;
+                        Task.Run(async () =>
+                        {
+                            await _server.Broadcast(Encoding.UTF8.GetBytes(text));
+                            Console.WriteLine("WebSocket server [{0}] broadcasts data -> [{1}].", _server.ListenedEndPoint, text);
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                _server.Stop().Wait();
+                Console.WriteLine("WebSocket server has been stopped on [{0}].", _server.ListenedEndPoint);
+            }
+            catch (Exception ex)
+            {
+                Logger.Get<Program>().Error(ex.Message, ex);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static async Task OnSessionStarted(AsyncWebSocketSession session)
+        {
+            Console.WriteLine(string.Format("WebSocket session [{0}] has connected [{1}].", session.RemoteEndPoint, session));
+            await Task.CompletedTask;
+        }
+
+        private static async Task OnSessionTextReceived(AsyncWebSocketSession session, string text)
+        {
+            Console.Write(string.Format("WebSocket session [{0}] received Text --> ", session.RemoteEndPoint));
+            Console.WriteLine(string.Format("{0}", text));
+
+            await session.Send(Encoding.UTF8.GetBytes("Echo -> " + text));
+        }
+
+        private static async Task OnSessionDataReceived(AsyncWebSocketSession session, byte[] data, int offset, int count)
+        {
+            var text = Encoding.UTF8.GetString(data, offset, count);
+            Console.Write(string.Format("WebSocket session [{0}] received Binary --> ", session.RemoteEndPoint));
+            Console.WriteLine(string.Format("{0}", text));
+
+            await session.Send(Encoding.UTF8.GetBytes("Echo -> " + text));
+        }
+
+        private static async Task OnSessionClosed(AsyncWebSocketSession session)
+        {
+            Console.WriteLine(string.Format("WebSocket session [{0}] has disconnected.", session.RemoteEndPoint));
+            await Task.CompletedTask;
+        }
+    }
+}
