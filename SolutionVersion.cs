c13617a5a8391336e158f6b466a37620f095a259
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-01-18 13:52:33 +0800

    update version

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.1.12.0")]
-[assembly: AssemblyFileVersion("1.1.12.0")]
+[assembly: AssemblyVersion("1.1.13.0")]
+[assembly: AssemblyFileVersion("1.1.13.0")]
 [assembly: ComVisible(false)]
