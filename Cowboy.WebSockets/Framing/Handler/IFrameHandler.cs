1671ab92b019620ece6a182557bcb6c91d4ace38
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-01-18 14:13:22 +0800

    want to add a frame handler to break the handling process

---
+++ Cowboy.WebSockets/Framing/Handler/IFrameHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Cowboy.WebSockets
+{
+    public interface IFrameHandler
+    {
+        byte[] EncodeFrame(byte[] payload, int offset, int count, OpCode opCode, bool isFinal, bool isMasked);
+        bool TryDecodeFrame(byte[] buffer, int count, bool isMasked, out int frameLength, out byte[] payload, out int payloadOffset, out int payloadCount);
+    }
+}
---
+++ Cowboy.WebSockets/Framing/Handler/LengthHeaderFrameHandler.cs
@@ -0,0 +1,45 @@
+﻿namespace Cowboy.WebSockets
+{
+    public class LengthHeaderFrameHandler : IFrameHandler
+    {
+        public LengthHeaderFrameHandler()
+        {
+        }
+
+        public byte[] EncodeFrame(byte[] payload, int offset, int count, OpCode opCode, bool isFinal, bool isMasked)
+        {
+            return Frame.Encode(opCode, payload, offset, count, isFinal, isMasked);
+        }
+
+        public bool TryDecodeFrame(byte[] buffer, int count, bool isMasked, out int frameLength, out byte[] payload, out int payloadOffset, out int payloadCount)
+        {
+            frameLength = 0;
+            payload = null;
+            payloadOffset = 0;
+            payloadCount = 0;
+
+            var frameHeader = Frame.DecodeHeader(buffer, count);
+            if (frameHeader != null && frameHeader.Length + frameHeader.PayloadLength <= count)
+            {
+                if (isMasked)
+                {
+                    payload = Frame.DecodeMaskedPayload(buffer, frameHeader.MaskingKeyOffset, frameHeader.Length, frameHeader.PayloadLength);
+                    payloadOffset = 0;
+                    payloadCount = payload.Length;
+                }
+                else
+                {
+                    payload = buffer;
+                    payloadOffset = frameHeader.Length;
+                    payloadCount = frameHeader.PayloadLength;
+                }
+
+                frameLength = frameHeader.Length + frameHeader.PayloadLength;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
