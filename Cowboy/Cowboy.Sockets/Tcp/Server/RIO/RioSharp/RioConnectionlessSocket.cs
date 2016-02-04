7f44eb7df332f4b2a6614d671a7fa94fbd5af56d
Dennis Gao <gaochundong@hotmail.com>
2016-02-04 22:20:26 +0800

    finally runnable after disabled the 'Prefer 32-bit' build flag

---
+++ Cowboy/Cowboy.Sockets/Tcp/Server/RIO/RioSharp/RioConnectionlessSocket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cowboy.Sockets.Experimental
+{
+    public class RioConnectionlessSocket : RioSocketBase
+    {
+        internal RioConnectionlessSocket(RioSocketPool pool, RioFixedBufferPool sendBufferPool, RioFixedBufferPool receiveBufferPool,
+            uint maxOutstandingReceive, uint maxOutstandingSend, IntPtr SendCompletionQueue, IntPtr ReceiveCompletionQueue) :
+            base(sendBufferPool, receiveBufferPool, maxOutstandingReceive, maxOutstandingSend,
+                SendCompletionQueue, ReceiveCompletionQueue,
+                ADDRESS_FAMILIES.AF_INET, SOCKET_TYPE.SOCK_DGRAM, PROTOCOL.IPPROTO_UDP)
+        {
+
+        }
+
+
+        public bool IsBroadcast { get; set; }
+    }
+}
---
+++ Cowboy/Cowboy.Sockets/Tcp/Server/RIO/RioSharp/RioConnectionlessSocketPool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cowboy.Sockets.Experimental
+{
+    public class RioConnectionlessSocketPool : RioSocketPool
+    {
+        public RioConnectionlessSocketPool(RioFixedBufferPool sendPool, RioFixedBufferPool revicePool, int socketCount,
+            uint maxOutstandingReceive = 1024, uint maxOutstandingSend = 1024, uint maxOutsandingCompletions = 1024)
+            : base(sendPool, revicePool, maxOutstandingReceive, maxOutstandingSend, maxOutsandingCompletions)
+        {
+
+        }
+
+        public RioConnectionlessSocket BindUdpSocket()
+        {
+            return new RioConnectionlessSocket(this,SendBufferPool,ReceiveBufferPool,MaxOutstandingReceive,MaxOutstandingSend,SendCompletionQueue,ReceiveCompletionQueue);           
+        }
+    }
+}
