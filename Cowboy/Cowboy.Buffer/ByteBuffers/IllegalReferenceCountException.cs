2a91e338a8587448a553e286279c2fa73508e6c1
Dennis Gao <gaochundong@hotmail.com>
2016-03-06 21:51:29 +0800

    update new files

---
+++ Cowboy/Cowboy.Buffer/ByteBuffers/IllegalReferenceCountException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cowboy.Buffer.ByteBuffers
+{
+    /// <summary>
+    /// Exception thrown during instances where a reference count is used incorrectly
+    /// </summary>
+    public class IllegalReferenceCountException : InvalidOperationException
+    {
+        public IllegalReferenceCountException(int count)
+            : base(string.Format("Illegal reference count of {0} for this object", count))
+        {
+        }
+
+        public IllegalReferenceCountException(int refCnt, int increment)
+            : base("refCnt: " + refCnt + ", " + (increment > 0 ? "increment: " + increment : "decrement: " + -increment))
+        {
+        }
+    }
+}
