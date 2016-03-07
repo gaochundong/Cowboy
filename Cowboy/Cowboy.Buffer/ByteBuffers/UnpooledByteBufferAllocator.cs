edde4326e426c4fe5803d45c5f5c7b504cfa97ae
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-03-07 20:05:03 +0800

    move code to new folders

---
+++ Cowboy/Cowboy.Buffer/ByteBuffers/UnpooledByteBufferAllocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cowboy.Buffer.ByteBuffers
+{
+    /// <summary>
+    /// Unpooled implementation of <see cref="IByteBufferAllocator"/>.
+    /// </summary>
+    public class UnpooledByteBufferAllocator : AbstractByteBufferAllocator
+    {
+        public static readonly UnpooledByteBufferAllocator Default = new UnpooledByteBufferAllocator();
+
+        protected override IByteBuffer NewBuffer(int initialCapacity, int maxCapacity)
+        {
+            return new UnpooledHeapByteBuffer(this, initialCapacity, maxCapacity);
+        }
+    }
+}
