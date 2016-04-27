fdd81fa21d166d83116d6699b0074d69197dd6e2
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-04-27 12:28:43 +0800

    upgrade dependent libs

--- Cowboy/SolutionVersion.cs
+++ Cowboy/SolutionVersion.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.2.15.0")]
-[assembly: AssemblyFileVersion("1.2.15.0")]
+[assembly: AssemblyVersion("1.2.16.0")]
+[assembly: AssemblyFileVersion("1.2.16.0")]
 [assembly: ComVisible(false)]
