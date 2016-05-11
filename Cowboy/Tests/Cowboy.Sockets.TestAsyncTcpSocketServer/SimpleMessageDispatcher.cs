f4293c87e411117e9c3bf90470cadc51e2bbfc71
gaochundong@hotmail.com <gaochundong@hotmail.com>
2016-05-11 11:34:43 +0800

    enable log when have simple tests

--- Cowboy/Tests/Cowboy.Sockets.TestAsyncTcpSocketServer/SimpleMessageDispatcher.cs
+++ Cowboy/Tests/Cowboy.Sockets.TestAsyncTcpSocketServer/SimpleMessageDispatcher.cs
@@ -8,22 +8,22 @@ namespace Cowboy.Sockets.TestAsyncTcpSocketServer
     {
         public async Task OnSessionStarted(AsyncTcpSocketSession session)
         {
-            //Console.WriteLine(string.Format("TCP session {0} has connected {1}.", session.RemoteEndPoint, session));
+            Console.WriteLine(string.Format("TCP session {0} has connected {1}.", session.RemoteEndPoint, session));
             await Task.CompletedTask;
         }
 
         public async Task OnSessionDataReceived(AsyncTcpSocketSession session, byte[] data, int offset, int count)
         {
             var text = Encoding.UTF8.GetString(data, offset, count);
-            //Console.Write(string.Format("Client : {0} --> ", session.RemoteEndPoint));
-            //Console.WriteLine(text);
+            Console.Write(string.Format("Client : {0} --> ", session.RemoteEndPoint));
+            Console.WriteLine(text);
 
             await session.SendAsync(Encoding.UTF8.GetBytes(text));
         }
 
         public async Task OnSessionClosed(AsyncTcpSocketSession session)
         {
-            //Console.WriteLine(string.Format("TCP session {0} has disconnected.", session));
+            Console.WriteLine(string.Format("TCP session {0} has disconnected.", session));
             await Task.CompletedTask;
         }
     }
--- Cowboy/Tests/Cowboy.Sockets.TestTcpSocketRioServer/SimpleMessageDispatcher.cs
+++ Cowboy/Tests/Cowboy.Sockets.TestTcpSocketRioServer/SimpleMessageDispatcher.cs
@@ -9,22 +9,22 @@ namespace Cowboy.Sockets.TestTcpSocketRioServer
     {
         public async Task OnSessionStarted(TcpSocketRioSession session)
         {
-            //Console.WriteLine(string.Format("TCP session has connected {0}.", session));
+            Console.WriteLine(string.Format("TCP session has connected {0}.", session));
             await Task.CompletedTask;
         }
 
         public async Task OnSessionDataReceived(TcpSocketRioSession session, byte[] data, int offset, int count)
         {
             var text = Encoding.UTF8.GetString(data, offset, count);
-            //Console.Write(string.Format("Client : --> "));
-            //Console.WriteLine(text);
+            Console.Write(string.Format("Client : --> "));
+            Console.WriteLine(text);
 
             await session.SendAsync(Encoding.UTF8.GetBytes(text));
         }
 
         public async Task OnSessionClosed(TcpSocketRioSession session)
         {
-            //Console.WriteLine(string.Format("TCP session {0} has disconnected.", session));
+            Console.WriteLine(string.Format("TCP session {0} has disconnected.", session));
             await Task.CompletedTask;
         }
     }
