90915b7dc8f25e6e106661e3cd7433789a776662
gaochundong <gaochundong@hotmail.com>
2016-10-22 15:18:43 +0800

    add brand new impressive buffer manager

---
+++ Cowboy/Cowboy.Sockets/Buffer/UnableToAllocateBufferException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cowboy.Buffer
+{
+    [Serializable]
+    public class UnableToAllocateBufferException : Exception
+    {
+        public UnableToAllocateBufferException()
+            : base("Cannot allocate buffer after few trials.")
+        {
+        }
+    }
+}
---
+++ Cowboy/Cowboy.Sockets/Buffer/UnableToCreateMemoryException .cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cowboy.Buffer
+{
+    [Serializable]
+    public class UnableToCreateMemoryException : Exception
+    {
+        public UnableToCreateMemoryException()
+            : base("All buffers were in use and acquiring more memory has been disabled.")
+        {
+        }
+    }
+}
