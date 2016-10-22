416002ab684df0ed81a50b75cead6fc11daacdeb
gaochundong <gaochundong@hotmail.com>
2016-10-22 14:34:14 +0800

    sometimes the user need to define buffer manager by themselves to adapt the usage of specified scenario

--- Cowboy/SolutionVersion.cs
+++ Cowboy/SolutionVersion.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
 [assembly: ComVisible(false)]
