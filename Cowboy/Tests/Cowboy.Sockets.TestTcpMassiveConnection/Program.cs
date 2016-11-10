58b5cc641ac156a1b9da0da9cc8c50aba5c33ba0
gaochundong <gaochundong@hotmail.com>
2016-11-10 15:58:04 +0800

    add many of connections test client

---
+++ Cowboy/Tests/Cowboy.Sockets.TestTcpMassiveConnection/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Logrila.Logging.NLogIntegration;
+
+namespace Cowboy.Sockets.TestTcpMassiveConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NLogLogger.Use();
+
+            Queue<TcpSocketClient> clients = new Queue<TcpSocketClient>();
+
+            Console.WriteLine("TCP massive connections test.");
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Type command to open or close connections to the server...");
+                    string text = Console.ReadLine().ToLowerInvariant();
+                    if (text == "quit")
+                    {
+                        break;
+                    }
+                    else if (text.StartsWith("open"))
+                    {
+                        var splitter = text.Split(' ');
+                        if (splitter.Length > 1)
+                        {
+                            string input = splitter[1];
+                            int count = 0;
+                            if (int.TryParse(input, out count))
+                            {
+                                for (int i = 0; i < count; i++)
+                                {
+                                    var client = OpenTcpSocketClient();
+                                    clients.Enqueue(client);
+                                }
+
+                                Console.WriteLine("Opened {0} TCP connections to the server, now {1} in total.",
+                                    count, clients.Count);
+                            }
+                        }
+                    }
+                    else if (text.StartsWith("close"))
+                    {
+                        var splitter = text.Split(' ');
+                        if (splitter.Length > 1)
+                        {
+                            string input = splitter[1];
+                            int count = 0;
+                            if (int.TryParse(input, out count))
+                            {
+                                int round = count;
+                                while (clients.Count > 0 && round > 0)
+                                {
+                                    var client = clients.Dequeue();
+                                    CloseTcpSocketClient(client);
+                                    round--;
+                                }
+
+                                Console.WriteLine("Closed {0} TCP connections to the server, now {1} in total.",
+                                    count - round, clients.Count);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static TcpSocketClient OpenTcpSocketClient()
+        {
+            var config = new TcpSocketClientConfiguration();
+            //config.UseSsl = true;
+            //config.SslTargetHost = "Cowboy";
+            //config.SslClientCertificates.Add(new System.Security.Cryptography.X509Certificates.X509Certificate2(@"D:\\Cowboy.cer"));
+            //config.SslPolicyErrorsBypassed = false;
+            //config.SendTimeout = TimeSpan.FromSeconds(2);
+
+            //config.FrameBuilder = new FixedLengthFrameBuilder(20000);
+            //config.FrameBuilder = new RawBufferFrameBuilder();
+            //config.FrameBuilder = new LineBasedFrameBuilder();
+            //config.FrameBuilder = new LengthPrefixedFrameBuilder();
+            //config.FrameBuilder = new LengthFieldBasedFrameBuilder();
+
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 65001);
+
+            var client = new TcpSocketClient(remoteEP, config);
+            client.ServerConnected += client_ServerConnected;
+            client.ServerDisconnected += client_ServerDisconnected;
+            client.ServerDataReceived += client_ServerDataReceived;
+            client.Connect();
+
+            return client;
+        }
+
+        private static void CloseTcpSocketClient(TcpSocketClient client)
+        {
+            client.ServerConnected -= client_ServerConnected;
+            client.ServerDisconnected -= client_ServerDisconnected;
+            client.ServerDataReceived -= client_ServerDataReceived;
+            client.Close();
+        }
+
+        static void client_ServerConnected(object sender, TcpServerConnectedEventArgs e)
+        {
+            Console.WriteLine(string.Format("TCP server {0} has connected.", e.RemoteEndPoint));
+        }
+
+        static void client_ServerDisconnected(object sender, TcpServerDisconnectedEventArgs e)
+        {
+            Console.WriteLine(string.Format("TCP server {0} has disconnected.", e.RemoteEndPoint));
+        }
+
+        static void client_ServerDataReceived(object sender, TcpServerDataReceivedEventArgs e)
+        {
+            var text = Encoding.UTF8.GetString(e.Data, e.DataOffset, e.DataLength);
+            Console.Write(string.Format("Server : {0} --> ", e.Client.RemoteEndPoint));
+            if (e.DataLength < 1024 * 1024 * 1)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("{0} Bytes", e.DataLength);
+            }
+        }
+    }
+}
