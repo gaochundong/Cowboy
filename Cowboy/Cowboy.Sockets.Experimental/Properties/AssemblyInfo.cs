ab25a785582ad6be6e8c2580372f281a64c809ae
gaochundong <gaochundong@hotmail.com>
2017-04-26 13:26:55 +0800

    move the rio out of the main project

---
+++ Cowboy/Cowboy.Sockets.Experimental/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cowboy.Sockets.Experimental")]
+[assembly: Guid("0d6991ef-2781-4185-8b36-80995c9800f3")]
--- Cowboy/Cowboy.Sockets/TplExtensions.cs
+++ Cowboy/Cowboy.Sockets/TplExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace Cowboy.Sockets
 {
-    internal static class TplExtensions
+    public static class TplExtensions
     {
         public static void Forget(this Task task) { }
     }
