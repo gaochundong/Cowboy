1d3d720e78eb7ed8a4832bfd0b57139641625c13
gaochundong <gaochundong@hotmail.com>
2017-04-26 13:29:32 +0800

    update project version and copyright

--- Cowboy/SolutionVersion.cs
+++ Cowboy/SolutionVersion.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Cowboy is a C# library for building sockets based services.")]
 [assembly: AssemblyCompany("Dennis Gao")]
 [assembly: AssemblyProduct("Cowboy")]
-[assembly: AssemblyCopyright("Copyright © 2015-2016 Dennis Gao")]
+[assembly: AssemblyCopyright("Copyright © 2015-2017 Dennis Gao")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
